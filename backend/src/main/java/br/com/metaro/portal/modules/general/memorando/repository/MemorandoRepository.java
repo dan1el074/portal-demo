e684a6fa028f93afae00766be944960f84d6d693
Daniel Vargas <daniel.rvargas10@gmail.com>
2026-05-15 12:04:01 -0300

    feat: add scheduler for send e-mail and notifications

--- backend/src/main/java/br/com/metaro/portal/modules/general/memorando/repository/MemorandoRepository.java
+++ backend/src/main/java/br/com/metaro/portal/modules/general/memorando/repository/MemorandoRepository.java
@@ -2,8 +2,18 @@ package br.com.metaro.portal.modules.general.memorando.repository;
 
 import br.com.metaro.portal.modules.general.memorando.entities.Memorando;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemorandoRepository extends JpaRepository<Memorando, Long> {
+    @Query("""
+        SELECT DISTINCT m
+        FROM Memorando m
+        LEFT JOIN FETCH m.fromDepartments d
+        WHERE m.id = :id
+    """)
+    public Optional<Memorando> findByIdWithDepartments(Long id);
 }
