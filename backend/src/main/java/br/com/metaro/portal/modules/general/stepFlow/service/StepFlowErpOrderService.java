31d6391c0cb50254e4d34a4a7df40683d02c939a
Daniel Vargas <daniel.rvargas10@gmail.com>
2026-07-28 17:14:49 -0300

    fix: minor bugs

--- backend/src/main/java/br/com/metaro/portal/modules/general/stepFlow/service/StepFlowErpOrderService.java
+++ backend/src/main/java/br/com/metaro/portal/modules/general/stepFlow/service/StepFlowErpOrderService.java
@@ -1,5 +1,7 @@
 package br.com.metaro.portal.modules.general.stepFlow.service;
 
+import br.com.metaro.portal.core.entities.User;
+import br.com.metaro.portal.core.services.UserService;
 import br.com.metaro.portal.core.services.exceptions.ResourceNotFoundException;
 import br.com.metaro.portal.core.services.exceptions.UnprocessableEntityException;
 import br.com.metaro.portal.modules.general.stepFlow.entities.Order;
@@ -8,6 +10,7 @@ import br.com.metaro.portal.modules.general.stepFlow.repositories.OrderRepositor
 import br.com.metaro.portal.util.erp.ErpOrderQueryService;
 import br.com.metaro.portal.util.erp.dto.ErpOrderDto;
 import br.com.metaro.portal.util.erp.dto.ErpOrderItemDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,26 +18,33 @@ import java.util.List;
 
 @Service
 public class StepFlowErpOrderService {
+    @Autowired
+    private UserService userService;
     private final ErpOrderQueryService erpOrderQueryService;
     private final OrderRepository orderRepository;
 
-    public StepFlowErpOrderService(
-            ErpOrderQueryService erpOrderQueryService,
-            OrderRepository orderRepository
-    ) {
+    public StepFlowErpOrderService(ErpOrderQueryService erpOrderQueryService, OrderRepository orderRepository) {
         this.erpOrderQueryService = erpOrderQueryService;
         this.orderRepository = orderRepository;
     }
 
     @Transactional(readOnly = true)
     public ErpOrderDto findAvailableOrderByNumber(int orderNumber) {
-        ErpOrderDto orderFromErp = erpOrderQueryService
-                .findProductionOrderByNumber(orderNumber)
+        User me = userService.authenticate();
+        boolean isAdmin = me.getAuthorities().stream().anyMatch(a -> a
+                .getAuthority().equals("ROLE_ADMIN"));
+
+        ErpOrderDto orderFromErp;
+
+        if (isAdmin) {
+            orderFromErp = erpOrderQueryService.findProductionOrderByNumberWithoutRules(orderNumber)
                 .orElseThrow(ResourceNotFoundException::new);
-        List<Order> localOrders = orderRepository.findByNumber(
-                orderNumber,
-                OrderStatus.CANCELLED
-        );
+        } else {
+            orderFromErp = erpOrderQueryService.findProductionOrderByNumber(orderNumber)
+                .orElseThrow(ResourceNotFoundException::new);
+        }
+
+        List<Order> localOrders = orderRepository.findByNumber(orderNumber, OrderStatus.CANCELLED);
 
         addLocallyProducedQuantities(orderFromErp, localOrders);
         ensureOrderHasPendingProduction(orderFromErp);
@@ -42,10 +52,7 @@ public class StepFlowErpOrderService {
         return orderFromErp;
     }
 
-    private void addLocallyProducedQuantities(
-            ErpOrderDto orderFromErp,
-            List<Order> localOrders
-    ) {
+    private void addLocallyProducedQuantities( ErpOrderDto orderFromErp,  List<Order> localOrders) {
         for (Order localOrder : localOrders) {
             for (ErpOrderItemDto itemFromErp : orderFromErp.getItems()) {
                 Integer producedQuantity = localOrder.getItems()
@@ -65,10 +72,6 @@ public class StepFlowErpOrderService {
                 .stream()
                 .allMatch(item -> item.getProducedQuantity().equals(item.getQuantity()));
 
-        if (fullyProduced) {
-            throw new UnprocessableEntityException(
-                    "Pedido não disponível para produção!"
-            );
-        }
+        if (fullyProduced) throw new UnprocessableEntityException("Pedido não disponível para produção!");
     }
 }
